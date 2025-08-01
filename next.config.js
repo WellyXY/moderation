dc0b816b1737912fb841eac823da432702d0fea2
Julián David <48492084+hipdev@users.noreply.github.com>
2025-07-31 23:27:19 -0500

    add biome, remove eslint packages, format files

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['commondatastorage.googleapis.com'],
-  },
-  // Removed experimental.appDir as it's no longer needed in Next.js 14
+	images: {
+		domains: ["commondatastorage.googleapis.com"],
+	},
+	// Removed experimental.appDir as it's no longer needed in Next.js 14
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
